f8997cbb03de6b6a949e0a26c2a3751eb860c40e
Connor Boulton <cjboulton22@gmail.com>
2021-10-21 14:07:54 -0600

    changes

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, "../index.html"))
 })
 
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/styles.css'))
+})
+
 const port = process.env.PORT || 4545
 
 app.listen(port, () => console.log(`port ${port} is running`))
